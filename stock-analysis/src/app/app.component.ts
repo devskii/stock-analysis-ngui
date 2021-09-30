d1c4678d3f614d9240534f422155d12588dcf0f1
Spencer Michalski <semski13@gmail.com>
2021-09-29 20:33:10 -0400

    Add fundamentals logic & current ratio

--- stock-analysis/src/app/app.component.ts
+++ stock-analysis/src/app/app.component.ts
@@ -8,7 +8,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'stock-analysis';
 
-  onSubmit(input: string) {
-    console.log(input);
+  goodSize = false;
+  prominent = false;
+  goodDividend = false;
+  debtToEquityRatio: number = -1;
+  totalCurrentAssets: number = -1;
+  totalCurrentLiabilities: number = -1;
+
+  setLargeCap(cap: boolean) {
+    this.goodSize = cap;
   }
+
+  setProminent(bool: boolean) {
+    this.prominent = bool;
+  }
+
+  setGoodDividend(bool: boolean) {
+    this.goodDividend = bool;
+  }
+
+  setDebtToEquityRatio(event: any) {
+    this.debtToEquityRatio = event.target.value;
+  }
+
+  setAssets(event: any) {
+    this.totalCurrentAssets = event.target.value || -1;
+  }
+
+  setLiabilities(event: any) {
+    this.totalCurrentLiabilities = event.target.value || -1;
+  }
+
+  constructor() {}
 }
